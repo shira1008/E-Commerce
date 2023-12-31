8af8906da6c045b754548484dda317be09367728
shira1008 <shaharshira1008@gmail.com>
2023-12-31 18:06:32 +0200

    started the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
 import React from 'react';
 
 const App = () => {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To the Shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
