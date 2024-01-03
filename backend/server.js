dd2a374eadc570790cd5ef25e35569de91362f06
shira1008 <shaharshira1008@gmail.com>
2024-01-03 13:11:42 +0200

    added nodemon and cur

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
-
-const port = 5000;
+import products from './data/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('API is running :) ');
 });
 
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 app.listen(port, () => console.log(`server is running on ${port} `));
